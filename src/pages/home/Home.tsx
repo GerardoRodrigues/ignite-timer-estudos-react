b7977d8da2d8963c8f7ef07ba7a01145ba9ebfbf
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-02-11 10:01:04 -0300

    pagina home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,37 @@
+import { Play } from "phosphor-react";
+import { Button, CountdownContainer, FormContainer, HomeContainer, MinutesInput, Separator, TaskInput } from "./styles";
+
 export function Home() {
     return(
-        <h1>HOME</h1>
+        <HomeContainer>
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput id="task" list="list-input" type="text" placeholder="Dê um nome ao seu projeto" />
+                    <datalist id="list-input">
+                        <option value="Prejeto 1" />
+                        <option value="Prejeto 2" />
+                        <option value="Prejeto 3" />
+                    </datalist>
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutesInput id="minutesAmount" type="number" placeholder="00" step={5} min={5} max={60} />
+                    <span>minutos.</span>
+                </FormContainer>
+            
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <Button type="submit">
+                    <Play size={24}/>
+                    Começar
+                </Button>
+            </form>
+        </HomeContainer>
     )
 }
