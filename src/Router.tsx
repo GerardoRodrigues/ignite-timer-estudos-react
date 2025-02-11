51c6d3b077019e5763809fbc241a496df5e631c5
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-02-11 08:14:11 -0300

    Layout das rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { History } from "./pages/history/History";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/history" element={<History />}></Route>
+      <Route path="/" element={<DefaultLayout/>}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/history" element={<History />}></Route>
+      </Route>
     </Routes>
   );
 }
